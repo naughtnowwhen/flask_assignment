e551a8e2647e98ab90be9e77ca106ec090758f39
naughtnowwhen <naughtnowwhen@gmail.com>
2019-08-12 13:14:40 -0700

    'working'

--- server.py
+++ server.py
@@ -6,12 +6,20 @@ app = Flask(__name__)
 def test():
     return render_template('index.html')
 
+@app.route('/play/', defaults={'times' : '3', 'color' : 'teal'})
+@app.route('/play/<times>/')
+@app.route('/play/<times>/<color>')
+def play(times=3, color="teal"):
+    return render_template('index.html',times=int(times), color = color)
+
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
   app.run(debug=True)
 
 # from flask import Flask  # Import Flask to allow us to create our app
 # # Create a new instance of the Flask class called "app"
 # app = Flask(__name__)
+
+
 
 
 # # The "@" decorator associates this route with the function immediately following
